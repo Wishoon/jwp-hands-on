54e6bc8a2701b7597f177f879a547491a08af558
Wishoon <heonga2@gmail.com>
2022-09-02 14:25:03 +0900

    feat: 1단계 - gzip과 같은 HTTP 압축 알고리즘을 적용할 수 있도록 설정

--- cache/src/main/java/com/example/cachecontrol/CacheWebConfig.java
+++ cache/src/main/java/com/example/cachecontrol/CacheWebConfig.java
@@ -1,13 +1,31 @@
 package com.example.cachecontrol;
 
+import java.util.concurrent.TimeUnit;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.mvc.WebContentInterceptor;
 
 @Configuration
 public class CacheWebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
+        WebContentInterceptor interceptor = new WebContentInterceptor();
+        interceptor.addCacheMapping(CacheControl.maxAge(60, TimeUnit.SECONDS)
+            .noCache()
+            .cachePrivate(),
+            "/**");
+        registry.addInterceptor(interceptor);
+    }
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("resources/**")
+            .addResourceLocations("/resources/")
+            .setCacheControl(CacheControl
+                .noCache().cachePrivate());
     }
 }
